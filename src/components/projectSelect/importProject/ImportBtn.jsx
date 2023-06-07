a649a38b8d1387096520029418e82f91b08457d9
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-07 06:50:33 -0700

    only allow import of json files

--- src/components/projectSelect/importProject/ImportBtn.jsx
+++ src/components/projectSelect/importProject/ImportBtn.jsx
@@ -1,7 +1,29 @@
-import { Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Upload, message } from 'antd';
 
 const ImportBtn = () => {
-  return <Button disabled>Import Checklist</Button>;
+  const props = {
+    name: 'file',
+    beforeUpload: (file) => {
+      const isJSON = file.type === 'application/json';
+      if (!isJSON) {
+        message.error(`${file.name} is not a json file`);
+      }
+      return isJSON || Upload.LIST_IGNORE;
+    },
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+  };
+
+  console.log(props.file);
+
+  return (
+    <Upload {...props} maxCount={1} accept='.json'>
+      <Button icon={<UploadOutlined />}>Import Checklist (JSON)</Button>
+    </Upload>
+  );
 };
 
 export default ImportBtn;
