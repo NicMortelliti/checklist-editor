68d1af2f17c0cb0bd024ec2ffce003921dde6e79
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-11 11:57:19 -0700

    add new index to state

--- src/components/edit/IndexSelection/NewIndexForm.jsx
+++ src/components/edit/IndexSelection/NewIndexForm.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNewIndexItem } from '../../../redux/dataSlice';
 
 export const NewIndexForm = (setFormIsOpen) => {
+  const dispatch = useDispatch();
+  const [formState, setFormState] = useState({ text: '' });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormState({ ...formState, text: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    () => setFormIsOpen(false);
+    dispatch(addNewIndexItem(formState));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor='indexTitle'>Index Title</label>
-      <input type='text' name='indexTitle' />
+      <input
+        type='text'
+        name='indexTitle'
+        value={formState.text}
+        onChange={(e) => handleChange(e)}
+      />
+      <input type='submit' value='Submit' />
       <button onClick={() => setFormIsOpen(false)}>Cancel</button>
     </form>
   );
--- src/redux/dataSlice.js
+++ src/redux/dataSlice.js
@@ -1,39 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuid } from 'uuid';
 
+const indexTemplate = {
+  id: uuid(),
+  text: '',
+  type: 'index',
+  cas_message: '',
+  latchable: '',
+  auto_sensed_bool: '',
+  invert_sensed_bool: '',
+  auto_reset_bool: '',
+  timer_sec: '',
+  sensed_timer_bool: '',
+  synoptic_link: '',
+  children: [],
+};
+
 const initialState = {
   loading: false,
   error: null,
   selectedIndex: '',
   selectedSubIndex: '',
-  indexes: [
-    {
-      key: uuid(),
-      text: 'Normal',
-    },
-    {
-      key: uuid(),
-      text: 'Non-Normal',
-    },
-    {
-      key: uuid(),
-      text: 'Procedure',
-    },
-  ],
-  subIndexes: [
-    {
-      key: uuid(),
-      text: 'Preflight',
-    },
-    {
-      key: uuid(),
-      text: 'Taxi',
-    },
-    {
-      key: uuid(),
-      text: 'Takeoff',
-    },
-  ],
   data: [],
 };
 
@@ -63,6 +50,7 @@ export const dataSlice = createSlice({
     }),
     addNewIndexItem: (state, action) => ({
       ...state,
+      data: [...state.data, { ...indexTemplate, ...action.payload }],
     }),
   },
 });
