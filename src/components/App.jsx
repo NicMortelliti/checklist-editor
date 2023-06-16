7e3046b12fda458995b0c26dcb26310f37bbbf0c
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-16 14:09:29 -0700

    prevent collapsing rows when modal opens

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProjectSelect } from './projectSelect/ProjectSelect';
 import { EditingInterface } from './edit/EditingInterface';
 import { useSelector } from 'react-redux';
@@ -5,14 +6,15 @@ import { ModalConductor } from './edit/modals';
 
 export const App = () => {
   const { onBoarding } = useSelector((state) => state.ui);
-  const RenderInterface = () => {
+
+  const RenderInterface = useMemo(() => {
     if (onBoarding) return <ProjectSelect />;
     return <EditingInterface />;
-  };
+  }, [onBoarding]);
 
   return (
     <>
-      <RenderInterface />
+      {RenderInterface}
       <ModalConductor />
     </>
   );
