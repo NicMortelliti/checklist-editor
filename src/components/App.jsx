6f1ea572f72bccae3ec77a16fdd24e688f9d4d8a
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-16 19:43:05 -0700

    add grid template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { ProjectSelect } from './projectSelect/ProjectSelect';
 import { EditingInterface } from './edit/EditingInterface';
 import { useSelector } from 'react-redux';
 import { ModalConductor } from './edit/modals';
-import { Flex } from '@chakra-ui/react';
+import { Grid, GridItem, Flex } from '@chakra-ui/react';
 
 export const App = () => {
   const { onBoarding } = useSelector((state) => state.ui);
@@ -13,13 +13,40 @@ export const App = () => {
     return <EditingInterface />;
   }, [onBoarding]);
 
+  
+
   return (
-    <Flex
-      minHeight='100vh'
-      alignItems={onBoarding ? 'center' : 'top'}
-      justifyContent='center'>
-      {RenderInterface}
+    <>
+      <Grid
+        templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+        gridTemplateRows={'50px 1fr 30px'}
+        gridTemplateColumns={'150px 1fr'}
+        h='200px'
+        gap='1'
+        color='blackAlpha.700'
+        fontWeight='bold'>
+        <GridItem pl='2' bg='orange.300' area={'header'}>
+          Header
+        </GridItem>
+        <GridItem pl='2' bg='pink.300' area={'nav'}>
+          Nav
+        </GridItem>
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          <Flex
+            minHeight='100vh'
+            alignItems={onBoarding ? 'center' : 'top'}
+            justifyContent='center'>
+            {RenderInterface}
+          </Flex>
+        </GridItem>
+        <GridItem pl='2' bg='blue.300' area={'footer'}>
+          Footer
+        </GridItem>
+      </Grid>
+      ;
       <ModalConductor />
-    </Flex>
+    </>
   );
 };
