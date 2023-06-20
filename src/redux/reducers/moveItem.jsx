a1ddbe89074dec93c7bc6fd96de385aaa822a693
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-20 10:56:03 -0700

    add move row feature

--- src/redux/reducers/moveItem.jsx
+++ src/redux/reducers/moveItem.jsx
@@ -1,25 +1,43 @@
-export const moveItemInDataArray = (items, id, direction) => {
-  const newItemsArray = items.map((item) => {
-    if (item.id === id) {
-      const { children } = item;
-      let currentIndex = children.findIndex((child) => child.id === id);
-
-      if (direction === 'up' && currentIndex > 0) {
-        const itemToMove = children[currentIndex];
-        children.splice(currentIndex, 1);
-        children.splice(currentIndex - 1, 0, itemToMove);
-      } else if (direction === 'down' && currentIndex < children.length - 1) {
-        const itemToMove = children[currentIndex];
-        children.splice(currentIndex, 1);
-        children.splice(currentIndex + 1, 0, itemToMove);
+export const moveItemInDataArray = (data, id, direction) => {
+  // Recursive function to find the object with matching id
+  const findObject = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+      if (item.id === id) {
+        return { item, parent: arr };
+      } else if (item.children.length > 0) {
+        const result = findObject(item.children);
+        if (result) {
+          return result;
+        }
       }
     }
+    return null;
+  };
 
-    return {
-      ...item,
-      children: moveItemInDataArray(item.children, id, direction),
-    };
-  });
+  // Create a deep copy of the data array
+  const newData = JSON.parse(JSON.stringify(data));
 
-  return newItemsArray;
+  // Find the object to move within the newData array
+  const result = findObject(newData);
+
+  if (result) {
+    const { item, parent } = result;
+    const currentIndex = parent.indexOf(item);
+
+    // Move the item up if the direction is 'up' and it is not already at the top
+    if (direction === 'up' && currentIndex > 0) {
+      const newIndex = currentIndex - 1;
+      parent.splice(currentIndex, 1); // Remove the item from its current position
+      parent.splice(newIndex, 0, item); // Insert the item at the new position
+    }
+    // Move the item down if the direction is 'down' and it is not already at the bottom
+    else if (direction === 'down' && currentIndex < parent.length - 1) {
+      const newIndex = currentIndex + 1;
+      parent.splice(currentIndex, 1); // Remove the item from its current position
+      parent.splice(newIndex, 0, item); // Insert the item at the new position
+    }
+  }
+
+  return newData; // Return the modified newData array
 };
