109a3c803f28f3cf5bbd6a7041b11f02da21f812
Nic Mortelliti <npmortelliti@gmail.com>
2023-07-05 14:25:02 -0700

    remove test component

--- src/components/projectSelect/ProjectSelect.jsx
+++ src/components/projectSelect/ProjectSelect.jsx
@@ -3,19 +3,10 @@ import { ImportBtn } from './ImportBtn';
 import { Flex, HStack, Text, VStack } from '@chakra-ui/react';
 import { theme } from '../../styles/theme';
 
-import { IconButton } from '@chakra-ui/react';
-import { RxCross1 } from 'react-icons/rx';
-
 export const ProjectSelect = () => {
   return (
     <Flex minHeight='100vh' alignItems='center' justifyContent='center'>
       <VStack spacing={3}>
-        <IconButton
-          icon={<RxCross1 />}
-          size='xs'
-          variant='blue'
-          onClick={(e) => console.log(e)}
-        />
         <HStack mb='8px'>
           <Text fontSize='4xl' fontFamily={theme.fonts.heading}>
             Welcome to
